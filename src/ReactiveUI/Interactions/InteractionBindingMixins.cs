30e0c7240959d74c313be4ee100c5bd14eb293ee
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-07-10 08:09:42 -0500

    changed HandleInteraction

--- src/ReactiveUI/Interactions/InteractionBindingMixins.cs
+++ src/ReactiveUI/Interactions/InteractionBindingMixins.cs
@@ -1,19 +1,37 @@
+using System;
+using System.Linq.Expressions;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+using Splat;
+
 namespace ReactiveUI
 {
     public interface IInteractionBinderImplementation : IEnableLogger
     {
-        IReactiveBinding<TView, TViewModel, TRegistration> BindInteraction<TView, TViewModel, TRegistration, TOut>()
+        /// <summary>
+        /// Binds the interaction.
+        /// </summary>
+        /// <typeparam name="TView">The type of the view.</typeparam>
+        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+        /// <typeparam name="TProp">The type of the property.</typeparam>
+        /// <typeparam name="TInput">The type of the input.</typeparam>
+        /// <typeparam name="TOutput">The type of the output.</typeparam>
+        /// <param name="view">The view.</param>
+        /// <param name="viewModel">The view model.</param>
+        /// <param name="handler">The handler.</param>
+        /// <returns>
+        /// A reactive binding.
+        /// </returns>
+        IReactiveBinding<TView, TViewModel, TProp> BindInteraction<TView, TViewModel, TProp, TInput, TOutput>(
+            TView view,
+            TViewModel viewModel,
+            Expression<Func<InteractionContext<TInput, TOutput>, Task>> handler)
             where TViewModel : class
             where TView : class, IViewFor<TViewModel>
-            where TRegistration : Expression<Func<InteractionContext<TInput, TOutput>, Task<TOut>>>;
-        
-        IReactiveBinding<TView, TViewModel, TRegistration> BindInteraction<TView, TViewModel, TRegistration, TOut>()
-            where TViewModel : class
-            where TView : class, IViewFor<TViewModel>
-            where TRegistration : Expression<Func<InteractionContext<TInput, TOutput>, IObservable<TOut>>>;
+            where TProp : Interaction<TInput, TOutput>;
     }
-    
-    public class InteractionBindingMixins
+
+    public static class InteractionBindingMixins
     {
         private static readonly IInteractionBinderImplementation binderImplementation;
 
@@ -23,29 +41,43 @@ namespace ReactiveUI
             binderImplementation = default(IInteractionBinderImplementation);
         }
 
-        public static IReactiveBinding<TView, TViewModel, TOut> BindInteraction<TViewModel, TView, TProp, TOut>(
+        /// <summary>
+        /// Binds the interaction.
+        /// </summary>
+        /// <typeparam name="TView">The type of the view.</typeparam>
+        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+        /// <typeparam name="TProp">The type of the property.</typeparam>
+        /// <typeparam name="TInput">The type of the input.</typeparam>
+        /// <typeparam name="TOutput">The type of the output.</typeparam>
+        /// <param name="view">The view.</param>
+        /// <param name="viewModel">The view model.</param>
+        /// <param name="handler">The handler.</param>
+        /// <returns></returns>
+        public static IReactiveBinding<TView, TViewModel, TProp> BindInteraction<TView, TViewModel, TProp, TInput, TOutput>(
             this TView view,
             TViewModel viewModel,
             Expression<Func<InteractionContext<TInput, TOutput>, Task>> handler)
+            where TViewModel : class
+            where TView : class, IViewFor<TViewModel>
+            where TProp : Interaction<TInput, TOutput>
         {
-            return binderImplementation.BindInteraction<TView, TViewModel, TProp>(view, viewModel, handler);
-
-            return new ReactiveBinding<TView, TViewModel, TProp>(
-                                                                 view,
+            return new ReactiveBinding<TView, TViewModel, TProp>(view,
                                                                  viewModel,
                                                                  handler,
-                                                                 vmExpression,
+                                                                 null,
                                                                  source,
                                                                  BindingDirection.OneWay,
-                                                                 bindingDisposable);
+                                                                 Disposable.Create(() => { }));
         }
-        
-        public static IReactiveBinding<TView, TViewModel, TOut> BindInteraction<TViewModel, TView, TProp, TOut>(
+
+        public static IReactiveBinding<TView, TViewModel, TProp> BindInteraction<TView, TViewModel, TProp, TInput, TOutput, T>(
             this TView view,
             TViewModel viewModel,
-            Expression<Func<InteractionContext<TInput, TOutput>, IObservable>> handler)
+            Expression<Func<InteractionContext<TInput, TOutput>, IObservable<T>>> handler)
+            where TViewModel : class
+            where TView : class, IViewFor<TViewModel>
+            where TProp : Interaction<TInput, TOutput>
         {
-            
             return new ReactiveBinding<TView, TViewModel, TProp>(
                                                                  view,
                                                                  viewModel,
--- src/ReactiveUI/Interactions/InteractionMixins.cs
+++ src/ReactiveUI/Interactions/InteractionMixins.cs
@@ -1,10 +1,35 @@
+using System;
+
 namespace ReactiveUI
 {
     public static class InteractionMixins
     {
-        public static IDisposable HandleInteraction<T, TInput, TOutput>(
-            this IObservable<T> @this,
+        /// <summary>
+        /// Handles the interaction.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <typeparam name="TInput">The type of the input.</typeparam>
+        /// <typeparam name="TOutput">The type of the output.</typeparam>
+        /// <param name="this">The this.</param>
+        /// <param name="interaction">The interaction.</param>
+        /// <param name="input">The input.</param>
+        /// <returns>An observable sequence of output.</returns>
+        public static IObservable<TOutput> HandleInteraction<TSource, TInput, TOutput>(
+            this IObservable<TSource> @this,
             Interaction<TInput, TOutput> interaction,
-            TInput input) => interaction.Handle(input).Subscribe();
+            TInput input) => interaction?.Handle(input) ?? throw new ArgumentNullException(nameof(interaction));
+
+        /// <summary>
+        /// Handles the interaction.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TInput">The type of the input.</typeparam>
+        /// <typeparam name="TOutput">The type of the output.</typeparam>
+        /// <param name="this">The this.</param>
+        /// <param name="interaction">The interaction.</param>
+        /// <returns>An observable sequence of output.</returns>
+        public static IObservable<TOutput> HandleInteraction<TSource, TInput, TOutput>(
+            this IObservable<TSource> @this,
+            Interaction<TInput, TOutput> interaction) => interaction?.Handle(default(TInput)) ?? throw new ArgumentNullException(nameof(interaction));
     }
 }
